7d4a17f20110a549af9b726219191917fe963b86
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-24 07:43:40 +0300

    Write [5~0-bubble_sort.c

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -1,10 +1,34 @@
 #include "sort.h"
 
 /**
- * bubble_sort - ...
+ * bubble_sort - bubble sort algorithm implementation
  *
- * @list: ...
+ * @array: array to be sorted
+ * @size: size of array
  */
 void bubble_sort(int *array, size_t size)
 {
+	short swap;
+	size_t i, j;
+	int tmp;
+
+	if (array == NULL)
+		return;
+	for (i = 0; i < size; i++)
+	{
+		swap = 0;
+		for (j = 0; j < size - 1; j++)
+		{
+			if (array[j] > array[j + 1])
+			{
+				swap = 1;
+				/*array[j] ^= array[j + 1] ^= array[j] ^= array[j + 1];*/
+				tmp = array[j];
+				array[j] = array[j + 1];
+				array[j + 1] = tmp;
+			}
+		}
+		if (swap == 0)
+			break;
+	}
 }
