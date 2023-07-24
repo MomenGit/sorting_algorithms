980a0d5f5f5c796cffdf4e5e805417ec4111d0a6
momengit <51290928+MomenGit@users.noreply.github.com>
2023-07-24 07:54:05 +0300

    Performance optimization

--- 0-bubble_sort.c
+++ 0-bubble_sort.c
@@ -8,27 +8,27 @@
  */
 void bubble_sort(int *array, size_t size)
 {
-	short swap;
+	short swapped;
 	size_t i, j;
 	int tmp;
 
 	if (array == NULL)
 		return;
-	for (i = 0; i < size; i++)
+	for (i = 0; i < size - 1; i++)
 	{
-		swap = 0;
-		for (j = 0; j < size - 1; j++)
+		swapped = 0;
+		for (j = 0; j < size - i - 1; j++)
 		{
 			if (array[j] > array[j + 1])
 			{
-				swap = 1;
+				swapped = 1;
 				/*array[j] ^= array[j + 1] ^= array[j] ^= array[j + 1];*/
 				tmp = array[j];
 				array[j] = array[j + 1];
 				array[j + 1] = tmp;
 			}
 		}
-		if (swap == 0)
+		if (swapped == 0)
 			break;
 	}
 }
